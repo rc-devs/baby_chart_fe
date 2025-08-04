a130250cc538ca9e4fb9f9fa5c57102df49f6ec2
rc <rc.devs9@gmail.com>
2025-08-03 20:29:51 -0500

    httpclient and router in constructor

--- baby_chart_fe/src/shared/services/authentication.service.ts
+++ baby_chart_fe/src/shared/services/authentication.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(){
     // set token to storage
