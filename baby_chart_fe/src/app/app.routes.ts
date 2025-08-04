8f237840ee95ea7a4ddd4a884b118b01ddb95665
rc <rc.devs9@gmail.com>
2025-08-03 20:47:37 -0500

    login route

--- baby_chart_fe/src/app/app.routes.ts
+++ baby_chart_fe/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./core/login/login.component').then((c) => c.LoginComponent)
+  }
+];
