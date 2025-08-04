6763ac6f72c301778b184901a64a6fb0dd3cdca3
rc <rc.devs9@gmail.com>
2025-08-03 20:33:34 -0500

    login logic; pass email and password, expect observable

--- baby_chart_fe/src/shared/services/authentication.service.ts
+++ baby_chart_fe/src/shared/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,14 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  login(){
-    // set token to storage
+  login(email:string, password:string): Observable<{token: string}>{
+    return this.http.post<{token: string}>('http://localhost:3000/login', {
+      email,
+      password
+    })
   }
+
+  // set token
 
   getToken(){
     // get token from storage
