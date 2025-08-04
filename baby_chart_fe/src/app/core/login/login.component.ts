7245b5646b9c744ba149c93fb73c0922151c73a0
rc <rc.devs9@gmail.com>
2025-08-03 20:14:22 -0500

    reactive form setup

--- baby_chart_fe/src/app/core/login/login.component.ts
+++ baby_chart_fe/src/app/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = new FormGroup({
+    email: new FormControl ([Validators.required, Validators.email]),
+    password: new FormControl(Validators.required)
+  })
 }
