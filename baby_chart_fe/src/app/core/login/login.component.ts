78d3003ce528acf8e5200e466ba6e3eefb0dd10d
rc <rc.devs9@gmail.com>
2025-08-03 20:44:56 -0500

    login handler logic; pass params, subscribe to observable, set token, on fail; alert, log errror

--- baby_chart_fe/src/app/core/login/login.component.ts
+++ baby_chart_fe/src/app/core/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../../shared/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(private authService: AuthenticationService, private router: Router){}
+
   loginForm = new FormGroup({
     email: new FormControl ([Validators.required, Validators.email]),
     password: new FormControl(Validators.required)
   })
+
+  loginHandler(){
+    this.authService.login(this.loginForm.value.email!, this.loginForm.value.password!).subscribe({
+      next: (res: any) => {
+        this.authService.setToken(res.token);
+        // this.router.navigate(['/dashboard']) //not currently a route
+      },
+      error: (error: any) => {
+        alert("Login error. Ensure you are using a valid username and password.")
+        console.error('Login error', error)
+      }
+    })
+  }
 }
