3578138508e92b14ac5019fa6e478959607f81e4
rc <rc.devs9@gmail.com>
2025-08-13 13:17:32 -0500

    conditional display of login/signup components

--- baby_chart_fe/src/app/app.component.ts
+++ baby_chart_fe/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
+import { AuthenticationService } from '../shared/services/authentication.service';
+import { SignUpComponent } from './core/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'baby_chart_fe';
+
+  constructor(public authService: AuthenticationService){}
 }
