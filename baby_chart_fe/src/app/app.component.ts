a9982cf847c6b8c1e3deebe58feff73d8ee29b6b
rc <rc.devs9@gmail.com>
2025-08-13 18:18:17 -0500

    remove unused imports

--- baby_chart_fe/src/app/app.component.ts
+++ baby_chart_fe/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { LoginComponent } from './core/login/login.component';
 import { AuthenticationService } from '../shared/services/authentication.service';
-import { SignUpComponent } from './core/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
