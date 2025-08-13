989855f5e1261e530f3d9d289998f6cde7f89bba
rc <rc.devs9@gmail.com>
2025-08-13 14:01:05 -0500

    reactive form set up

--- baby_chart_fe/src/app/core/sign-up/sign-up.component.ts
+++ baby_chart_fe/src/app/core/sign-up/sign-up.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../../shared/services/authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+constructor(private authService: AuthenticationService, private matSnackBar: MatSnackBar){}
 
+  signUpForm = new FormGroup({
+    username: new FormControl("", [Validators.required]),
+    password: new FormControl("",[Validators.required]),
+    password_confirmation: new FormControl("", [Validators.required]),
+  })
+
+  signUpHandler(){
+    this.authService.signUp(this.signUpForm.value.username!, this.signUpForm.value.password!, this.signUpForm.value.password_confirmation!).subscribe({
+      next: (res: any) => {
+        this.matSnackBar.open("Sign-up successful! You may now use your login credentials!", 'Close')
+        this.signUpForm.reset()
+      },
+      error: (error: any) => {
+        this.matSnackBar.open("There was some error during sign-up. Refer to the Credential Requirements, and try again.", 'Close')
+        console.error('Sign up error', error.message)
+      }
+    })
+  }
 }
