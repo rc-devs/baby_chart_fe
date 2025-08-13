5c6ece6b5ae3783bb7c5d64c0e42549e89d0838c
rc <rc.devs9@gmail.com>
2025-08-13 13:21:18 -0500

    logout button using public authService

--- baby_chart_fe/src/app/features/dashboard/dashboard.component.ts
+++ baby_chart_fe/src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../../shared/services/authentication.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  
+  constructor(public authService: AuthenticationService){}
 
 }
