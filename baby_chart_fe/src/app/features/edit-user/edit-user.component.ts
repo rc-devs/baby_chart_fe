699088963d87771ab044e69b033d30e9320c088b
rc <rc.devs9@gmail.com>
2025-08-17 16:39:47 -0500

    reactive form for edit, get user data for input in form on init

--- baby_chart_fe/src/app/features/edit-user/edit-user.component.ts
+++ baby_chart_fe/src/app/features/edit-user/edit-user.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../../../shared/services/user.service';
+import { User } from '../../../shared/models/user';
+import { AuthenticationService } from '../../../shared/services/authentication.service';
 
 @Component({
   selector: 'app-edit-user',
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.css'
 })
 export class EditUserComponent {
+  user = signal<User | null>(null); // should be in service
 
+  constructor(private userService: UserService, private authService: AuthenticationService){}
+
+  updateUserDataForm = new FormGroup({
+    user_name: new FormControl ( this.user()?.username, Validators.required),
+    email: new FormControl ( this.user()?.email, Validators.required),
+    name_first: new FormControl ( this.user()?.name_first, Validators.required),
+    name_last: new FormControl ( this.user()?.name_last, Validators.required),
+  })
+
+  // should be in service
+  ngOnInit(): void {
+    this.userService.loadCurrentUserIfLoggedIn(this.authService) //get user data
+    this.userService.currentUserSubject.subscribe((res) => this.user.set(res)) //assign user data to signal for display in html
+  }
+
+  editUserHandler(){
+    
+  }
 }
