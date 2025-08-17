f0833fa4c901602e1590cf140ad57da5fff7fa90
rc <rc.devs9@gmail.com>
2025-08-16 19:18:50 -0500

    use behaviorSubject to retrieve data from backen and display in html ( used an LLM rip :/ )

--- baby_chart_fe/src/app/features/user-profile/user-profile.component.ts
+++ baby_chart_fe/src/app/features/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { UserService } from '../../../shared/services/user.service';
+import { User } from '../../../shared/models/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit{
+  user = signal<User | null>(null);
+  userId!: number;
 
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.userService.currentUserSubject.subscribe((res) => this.user.set(res)) 
+    console.log(this.userService.currentUserSubject)
+    this.userService.currentUserSubject.subscribe(console.log)
+  }
 }
--- baby_chart_fe/src/shared/services/user.service.ts
+++ baby_chart_fe/src/shared/services/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+currentUserSubject = new BehaviorSubject<User | null>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  setCurrentUser(user: User | null) {
+    this.currentUserSubject.next(user);
+  }
+
+  getBootstrapData() {
+    return this.http.get(`${environment.apiUrl}/web/bootstrap`).pipe(
+      tap((data: any) => {
+        this.setCurrentUser(data.current_user);
+      })
+    );
+  }
 }
