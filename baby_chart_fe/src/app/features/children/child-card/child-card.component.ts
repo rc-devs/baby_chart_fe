9af63a9827eae887aa818021ed40837c34f0d4ce
rc <rc.devs9@gmail.com>
2025-08-18 10:13:40 -0500

    gen child card component

---
+++ baby_chart_fe/src/app/features/children/child-card/child-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-card',
+  imports: [],
+  templateUrl: './child-card.component.html',
+  styleUrl: './child-card.component.css'
+})
+export class ChildCardComponent {
+
+}
