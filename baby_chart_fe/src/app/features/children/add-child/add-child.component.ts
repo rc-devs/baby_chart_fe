e8a45f77ff65ac5ca18ba78c2e7808aa766e556c
rc <rc.devs9@gmail.com>
2025-08-18 11:52:12 -0500

    addChildHandler; submit form values, subscribe to response, reset form on success, route user to child cards on success

--- baby_chart_fe/src/app/features/children/add-child/add-child.component.ts
+++ baby_chart_fe/src/app/features/children/add-child/add-child.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChildService } from '../../../../shared/services/child.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-child',
@@ -9,8 +11,24 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class AddChildComponent {
 
+  constructor(private childService: ChildService, private router: Router){}
+
   addChildForm = new FormGroup({
     child_name: new FormControl ('', [Validators.required]),
     date_of_birth: new FormControl (null, [Validators.required])
   })
+
+  addChildHandler(){
+    this.childService.createChild(this.addChildForm.value.child_name!, this.addChildForm.value.date_of_birth!).subscribe({
+      next: (createdChild) => {
+        console.log(createdChild) 
+        this.addChildForm.reset(); // reset form (redundant on navigation, possibly dangerous without if user submits empty form?)
+        this.router.navigate(['/dashboard/children/child-card']); 
+      },
+      error(err){
+        alert("There was some sort of error")
+        console.log(err)
+      }
+    })
+  }
 }
