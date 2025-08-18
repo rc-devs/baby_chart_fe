e9a57c65249225fbad332afe58b9cc17d7f65055
rc <rc.devs9@gmail.com>
2025-08-18 10:08:55 -0500

    routerlink button

--- baby_chart_fe/src/app/features/children/children.component.ts
+++ baby_chart_fe/src/app/features/children/children.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-children',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './children.component.html',
   styleUrl: './children.component.css'
 })
