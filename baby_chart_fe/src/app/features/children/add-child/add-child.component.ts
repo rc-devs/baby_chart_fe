196201860ca5865713d8c4e0e4efd74dc33f07cd
rc <rc.devs9@gmail.com>
2025-08-18 10:00:15 -0500

    add child component

---
+++ baby_chart_fe/src/app/features/children/add-child/add-child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-child',
+  imports: [],
+  templateUrl: './add-child.component.html',
+  styleUrl: './add-child.component.css'
+})
+export class AddChildComponent {
+
+}
