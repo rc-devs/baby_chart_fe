c1f60707b9c424e720d03b44e61bc1d9caff2ffb
rc <rc.devs9@gmail.com>
2025-08-22 19:40:16 -0500

    add chart attribute to child

--- baby_chart_fe/src/shared/models/child.ts
+++ baby_chart_fe/src/shared/models/child.ts
@@ -1,6 +1,9 @@
+import { Chart } from "./chart";
+
 export class Child {
   id!: number;
   child_name!: string;
   date_of_birth!: Date;
-  user_id?: number
+  user_id?: number;
+  chart!: Chart;
 }
