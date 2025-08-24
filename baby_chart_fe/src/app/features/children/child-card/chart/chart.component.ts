a8327adfb6d5f89452466cca5a936423902cfb57
rc <rc.devs9@gmail.com>
2025-08-24 01:48:28 -0500

    implement onchanges, pass child id and utilize response to set chart

--- baby_chart_fe/src/app/features/children/child-card/chart/chart.component.ts
+++ baby_chart_fe/src/app/features/children/child-card/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, signal, SimpleChanges, WritableSignal } from '@angular/core';
 import { ChartService } from '../../../../../shared/services/chart.service';
 import { Chart } from '../../../../../shared/models/chart';
 import { Child } from '../../../../../shared/models/child';
@@ -10,12 +10,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.css'
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnChanges{
   @Input() child: Child | null = null;
-  
   chart: WritableSignal<Chart | null> = signal<Chart | null>(null);
 
-  constructor( private chartService: ChartService ){}
+  constructor(private chartService: ChartService){}
 
   ngOnInit(): void {
      console.log("child: ", this.child)
@@ -23,22 +22,29 @@ export class ChartComponent implements OnInit {
      console.log("Chart:", this.chart())
   }
 
-  showChartHandler(){
-    console.log('shochartHandler fire')
-   
-    console.log(this.child)
-    console.log(this.child?.chart) // anything involving chart shows up as undefined, despite chart being created when child is created
-    console.log(this.child?.chart.id)
-    console.log(this.child?.chart.entries)
-    // need to get chart id from child
-    this.chartService.showChart(this.chart()!.id).subscribe((res) => { 
-      console.log('shochart fire')
-      if (res){ 
-        this.chart.set(res)
-        console.log(res)
-      }
-    })
-
-    
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['child'] && this.child?.chart) {
+      this.chart.set(this.child.chart);
+      console.log("Chart updated:", this.chart());
+    }
   }
+
+  showChartHandler() {
+  if (!this.child?.id) {
+    console.error('Child ID is missing');
+    return;
+  }
+  
+  console.log('Fetching chart for child ID:', this.child.id);
+  
+  this.chartService.showChartByChildId(this.child.id).subscribe({
+    next: (res) => { 
+      this.chart.set(res);
+      console.log('Chart loaded successfully:', res);
+    },
+    error: (err) => {
+      console.error('Error loading chart:', err);
+    }
+  });
+ }
 }
