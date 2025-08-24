188f4b2b1741d8f771fdb8e3e6341c28723f3f72
rc <rc.devs9@gmail.com>
2025-08-24 00:31:58 -0500

    display chart on init. Having trouble, reference comment

--- baby_chart_fe/src/app/features/children/child-card/chart/chart.component.ts
+++ baby_chart_fe/src/app/features/children/child-card/chart/chart.component.ts
@@ -1,28 +1,38 @@
 import { Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { ChartService } from '../../../../../shared/services/chart.service';
 import { Chart } from '../../../../../shared/models/chart';
-import { UserService } from '../../../../../shared/services/user.service';
-import { AuthenticationService } from '../../../../../shared/services/authentication.service';
-import { ChildService } from '../../../../../shared/services/child.service';
 import { Child } from '../../../../../shared/models/child';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.css'
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
   @Input() child: Child | null = null;
   
   chart: WritableSignal<Chart | null> = signal<Chart | null>(null);
 
-  constructor( private chartService: ChartService, private userService: UserService, private authService: AuthenticationService ){}
+  constructor( private chartService: ChartService ){}
+
+  ngOnInit(): void {
+     console.log("child: ", this.child)
+     this.chart.set(this.child?.chart!)
+     console.log("Chart:", this.chart())
+  }
 
   showChartHandler(){
+    console.log('shochartHandler fire')
+   
+    console.log(this.child)
+    console.log(this.child?.chart) // anything involving chart shows up as undefined, despite chart being created when child is created
+    console.log(this.child?.chart.id)
+    console.log(this.child?.chart.entries)
     // need to get chart id from child
-
-    this.chartService.showChart(this.child!.chart.id).subscribe((res) => { 
+    this.chartService.showChart(this.chart()!.id).subscribe((res) => { 
+      console.log('shochart fire')
       if (res){ 
         this.chart.set(res)
         console.log(res)
