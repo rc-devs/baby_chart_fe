0809db3581c7eb6454a88ebead0cf683a9e33511
rc <rc.devs9@gmail.com>
2025-08-25 18:14:30 -0500

    apply authGuards to existing routes

--- baby_chart_fe/src/app/app.routes.ts
+++ baby_chart_fe/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../shared/guards/auth.guard';
 
 export const routes: Routes = [
   
@@ -14,30 +15,37 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () => import('./features/dashboard/dashboard.component').then((c) => c.DashboardComponent), 
+    canActivate: [authGuard],
     children: [
       {
         path: 'user-profile',
         loadComponent: () => import('./features/user-profile/user-profile.component').then((c) => c.UserProfileComponent),
+        canActivate: [authGuard]
       },
       {
         path: 'edit-user',
         loadComponent: () => import('./features/user-profile/edit-user/edit-user.component').then((c) => c.EditUserComponent),
+        canActivate: [authGuard]
       },
       {
         path: 'children',
         loadComponent: () => import('./features/children/children.component').then((c) => c.ChildrenComponent),
+        canActivate: [authGuard],
         children: [
            {
         path: 'add-child',
         loadComponent: () => import('./features/children/add-child/add-child.component').then((c) => c.AddChildComponent),
+        canActivate: [authGuard],
           },
           {
         path: 'child-card',
         loadComponent: () => import('./features/children/child-card/child-card.component').then((c) => c.ChildCardComponent),
+        canActivate: [authGuard],
         children: [
               {
             path: 'chart',
             loadComponent: () => import('./features/children/child-card/chart/chart.component').then((c) => c.ChartComponent),
+            canActivate: [authGuard]
               }
             ]
           }
