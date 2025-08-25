648558c3f1ce244c1b0a23ed9f067bd1da529574
rc <rc.devs9@gmail.com>
2025-08-25 18:23:28 -0500

    add route and adjust alert

--- baby_chart_fe/src/shared/guards/no-auth.guard.ts
+++ baby_chart_fe/src/shared/guards/no-auth.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
+  const router = inject(Router)
 
   if (authService.isLoggedIn()){
-    console.log('You cannot access this path while logged in.')
+    alert('You cannot access this path while logged in. Returning you to dashboard')
+    router.navigate(['/dashboard'])
     return false;
   } else {
     return true;
