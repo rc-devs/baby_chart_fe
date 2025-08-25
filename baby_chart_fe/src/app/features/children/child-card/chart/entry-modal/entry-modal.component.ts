f15bde071db2f5dd29325966a49b888c7ceb084d
rc <rc.devs9@gmail.com>
2025-08-25 18:57:31 -0500

    gen entry modal component

---
+++ baby_chart_fe/src/app/features/children/child-card/chart/entry-modal/entry-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-modal',
+  imports: [],
+  templateUrl: './entry-modal.component.html',
+  styleUrl: './entry-modal.component.css'
+})
+export class EntryModalComponent {
+
+}
