55edfbc7b9e1e2e4e4f8fa6f3f037ea32fffc5e3
rc <rc.devs9@gmail.com>
2025-08-25 18:19:25 -0500

    no-auth logic

--- baby_chart_fe/src/shared/guards/no-auth.guard.ts
+++ baby_chart_fe/src/shared/guards/no-auth.guard.ts
@@ -1,5 +1,14 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthenticationService);
+
+  if (authService.isLoggedIn()){
+    console.log('You cannot access this path while logged in.')
+    return false;
+  } else {
+    return true;
+  }
 };
