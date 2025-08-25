59d62666e1f2fc531d93bf229f9ee264eae6c898
rc <rc.devs9@gmail.com>
2025-08-25 18:11:40 -0500

    authGuard logic

--- baby_chart_fe/src/shared/guards/auth.guard.ts
+++ baby_chart_fe/src/shared/guards/auth.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthenticationService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()){
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+
 };
