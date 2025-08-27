24f5a6e378eb918a081d9bb36079ff4efcef96ed
rc <rc.devs9@gmail.com>
2025-08-26 20:24:25 -0500

    create entry method

--- baby_chart_fe/src/shared/services/chart.service.ts
+++ baby_chart_fe/src/shared/services/chart.service.ts
@@ -3,23 +3,24 @@ import { Chart } from '../models/chart';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { Entry } from '../models/entry';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartService {
 
-  chart: Chart | null = null
-
   constructor(private http: HttpClient) { }
 
   chart: Chart | null = null
 
   //show chart/entries
-
-
   showChartByChildId(childId: number) {
   console.log("Fetching chart for child ID:", childId);
   return this.http.get<Chart>(`${environment.apiUrl}/children/${childId}/chart`);
+ }
+
+ createEntry(childId:number, entry: Entry,){
+  return this.http.post<Entry>(`${environment.apiUrl}/children/${childId}/chart/entries`,{entry}) 
  }
 }
