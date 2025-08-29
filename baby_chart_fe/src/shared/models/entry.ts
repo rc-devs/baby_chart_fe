43a1b79d753a733d1f3c2910c03199abac63d3da
rc <rc.devs9@gmail.com>
2025-08-28 20:05:58 -0500

    update interface to match outgoing/incoming data

--- baby_chart_fe/src/shared/models/entry.ts
+++ baby_chart_fe/src/shared/models/entry.ts
@@ -1,24 +1,19 @@
 export interface Entry {
   id?: number;
   time: Date;
-  medicationBool: boolean;
-  medicationDetails?: {
-    medication: string;
-    };
-  bath?: boolean;
-  comments?: string;
-  feeding?: boolean;
-  feedingDetails?: {
+  medication: string | null;
+  bath: boolean;
+  comments: string;
+  feeding_attributes: {
     bottle: boolean;
     breast: boolean;
     amount: number;
-    };
-  diaper?: boolean;
-  diaperDetails?: {
+  } | null;
+  diaper_attributes: {
     dirty: boolean;
     wet: boolean;
-    color?: string;
-    consistency?: string;
-    comments?: string;
-    };
+    color: string;
+    consistency: string;
+    comments: string;
+  } | null;
 }
