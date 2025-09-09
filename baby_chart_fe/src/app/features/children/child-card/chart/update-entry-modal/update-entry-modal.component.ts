9ca1f8585c30bfb8e9338d487d38ceda24e43376
rc <rc.devs9@gmail.com>
2025-09-08 19:40:26 -0500

    gen updateEntryModal

---
+++ baby_chart_fe/src/app/features/children/child-card/chart/update-entry-modal/update-entry-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-entry-modal',
+  imports: [],
+  templateUrl: './update-entry-modal.component.html',
+  styleUrl: './update-entry-modal.component.css'
+})
+export class UpdateEntryModalComponent {
+
+}
