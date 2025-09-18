c557660036921c223af95c6689ec83e38534ebbd
rc <rc.devs9@gmail.com>
2025-09-18 18:57:53 -0500

    caregiver signal

--- baby_chart_fe/src/shared/services/caregiver.service.ts
+++ baby_chart_fe/src/shared/services/caregiver.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CaregiverService {
-
+  caregivers = signal<User[]>([])
   constructor() { }
 }
