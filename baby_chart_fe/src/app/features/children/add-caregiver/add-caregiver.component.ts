1657a113540613473838494a1f00febe6b80a46e
rc <rc.devs9@gmail.com>
2025-09-18 18:38:23 -0500

    gen add-caregiver component

---
+++ baby_chart_fe/src/app/features/children/add-caregiver/add-caregiver.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-caregiver',
+  imports: [],
+  templateUrl: './add-caregiver.component.html',
+  styleUrl: './add-caregiver.component.css'
+})
+export class AddCaregiverComponent {
+
+}
