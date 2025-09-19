88bd1bd2884c4d8636a7081ea7e9906de181391f
rc <rc.devs9@gmail.com>
2025-09-18 19:21:02 -0500

    implement onit and set caregivers array

--- baby_chart_fe/src/app/features/children/add-caregiver/add-caregiver.component.ts
+++ baby_chart_fe/src/app/features/children/add-caregiver/add-caregiver.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { User } from '../../../../shared/models/user';
+import { CaregiverService } from '../../../../shared/services/caregiver.service';
 
 @Component({
   selector: 'app-add-caregiver',
@@ -8,9 +9,16 @@ import { User } from '../../../../shared/models/user';
   templateUrl: './add-caregiver.component.html',
   styleUrl: './add-caregiver.component.css'
 })
-export class AddCaregiverComponent {
+export class AddCaregiverComponent implements OnInit {
+  caregivers = signal<User[]>([])
+
+  constructor(private caregiverService: CaregiverService){}
 
   caregiverForm = new FormGroup({
 
   })
+
+  ngOnInit(): void {
+   this.caregivers.set(this.caregiverService.caregivers); //set caregiver signal with caregiver array
+  }
 }
