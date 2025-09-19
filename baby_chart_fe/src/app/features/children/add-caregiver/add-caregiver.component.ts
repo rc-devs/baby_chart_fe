18ae4e62a20f7712a5d385fc48b955a56c6015c7
rc <rc.devs9@gmail.com>
2025-09-19 00:57:21 -0500

    mat snackbar messages, confirmation message

--- baby_chart_fe/src/app/features/children/add-caregiver/add-caregiver.component.ts
+++ baby_chart_fe/src/app/features/children/add-caregiver/add-caregiver.component.ts
@@ -5,6 +5,7 @@ import { CaregiverService } from '../../../../shared/services/caregiver.service'
 import { ChildService } from '../../../../shared/services/child.service';
 import { Child } from '../../../../shared/models/child';
 import { UserService } from '../../../../shared/services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-caregiver',
@@ -17,7 +18,7 @@ export class AddCaregiverComponent implements OnInit {
   children = signal<Child[]>([])
   user = []
 
-  constructor(private caregiverService: CaregiverService, private childService: ChildService, private userService: UserService){}
+  constructor(private caregiverService: CaregiverService, private childService: ChildService, private userService: UserService, private matSnackBar: MatSnackBar){}
 
   caregiverForm = new FormGroup({
     userId: new FormControl<number | null>(null, Validators.required),
@@ -42,18 +43,33 @@ export class AddCaregiverComponent implements OnInit {
 
 addCaregiverHandler(){
   if (this.caregiverForm.valid) {
+    //ids from form 
     const userId = Number(this.caregiverForm.value.userId);
     const childId = Number(this.caregiverForm.value.childId);
-
     console.log('User ID:', userId);
     console.log('Child ID:', childId);
 
+    //select user/child from arrays
+    const selectedUser = this.caregivers().find(user => user.id === userId);
+    const selectedChild = this.children().find(child => child.id === childId);
+
+    //assign names for use in messages
+    const userName = selectedUser?.username ?? 'Unknown User';
+    const childName = selectedChild?.child_name ?? 'Unknown Child';
+
+    let userConfirmed = confirm(
+      `Are you sure you want to give ${userName} access to view, edit, and delete entries for ${childName}`
+    );
+
+    if (userConfirmed){
+    //pass ids to create access
     this.caregiverService.addAccessToCaregiver(userId, childId).subscribe({
-        next: () => console.log('Caregiver access added successfully'),
+        next: () => (this.matSnackBar.open(`Caregiver access for ${childName} successfully given to ${userName}`, 'Close'), this.caregiverForm.reset()), 
         error: (err) => console.error('Error adding caregiver access:', err)
       });
-  } else {
-    console.log('there was some error')
+    } else {
+      this.matSnackBar.open(`${userName} Access to ${childName} denied.`, 'Close'), this.caregiverForm.reset()
+    }
   }
  }
 }
