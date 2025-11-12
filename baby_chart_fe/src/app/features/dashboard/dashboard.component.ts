52d79f4591d9f7ea500d862e61b2e014e0ac19c1
rc <rc.devs9@gmail.com>
2025-11-12 15:27:28 -0600

    mat-icons and button layout

--- baby_chart_fe/src/app/features/dashboard/dashboard.component.ts
+++ baby_chart_fe/src/app/features/dashboard/dashboard.component.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 import { AuthenticationService } from '../../../shared/services/authentication.service';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { UserService } from '../../../shared/services/user.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, RouterModule ],
+  imports: [RouterOutlet, RouterModule, MatIconModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  
-  constructor(public authService: AuthenticationService, private userService: UserService){}
-  
-  ngOnInit(): void {
-    this.userService.loadCurrentUserIfLoggedIn(this.authService)// get user data
-  } 
+  constructor(
+    public authService: AuthenticationService,
+    private userService: UserService
+  ) {}
 
+  ngOnInit(): void {
+    this.userService.loadCurrentUserIfLoggedIn(this.authService); // get user data
+  }
 }
